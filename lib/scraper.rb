375777ff0431cec4f07ce6964cab54fbcaaf36b6
Ramesh <rameshnaidu.allu@gmail.com>
2020-05-14 00:01:31 +0530

    job search based on input string

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,17 +6,18 @@ require 'json'
 class Scraper
   attr_accessor :data
 
-  def initialize
-    doc = Nokogiri::XML(URI.open('https://stackoverflow.com/jobs?q=ruby&r=true'))
+  def initialize(tech)
+    @tech=tech.downcase
+    doc = Nokogiri::XML(URI.open("https://stackoverflow.com/jobs?q=#{@tech}&r=true"))
     @data = doc.xpath('//*[contains(concat( " ", @class, " " ),
                concat( " ", "stretched-link", " " ))]')
   end
 
   def json_data
     json = JSON.pretty_generate(@data.map { |link| {}.merge('title' => (link['title']).to_s, 'link' => "https://stackoverflow.com#{link['href']}") })
-    File.open('doc/sample.json', 'w').write(json)
+    File.open("doc/#{@tech}-remote.json", 'w').write(json)
   end
 end
 
-jobs = Scraper.new
+jobs = Scraper.new("ruby on rails")
 jobs.json_data
