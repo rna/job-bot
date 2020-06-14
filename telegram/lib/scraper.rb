3a3b694cf9efc51ec885ca74ce9445a82a2a8eaf
Ramesh <rameshnaidu.allu@gmail.com>
2020-06-15 01:14:36 +0530

    convert extracted data to json object

--- telegram/lib/scraper.rb
+++ telegram/lib/scraper.rb
@@ -8,21 +8,29 @@ class Scraper
 
   def initialize(tech)
     @tech = tech
-    doc = Nokogiri::XML(URI.open("https://stackoverflow.com/jobs?q=#{@tech}&r=true"))
-    @data = doc.xpath('//*[contains(concat( " ", @class, " " ),
+    @doc = Nokogiri::XML(URI.open("https://stackoverflow.com/jobs?q=#{@tech}&r=true"))
+    @data = @doc.xpath('//*[contains(concat( " ", @class, " " ),
                concat( " ", "stretched-link", " " ))]')
   end
 
   def extract_data
-    # @data.map { |link| {}.merge('title' => (link['title']).to_s, 'link' => "https://stackoverflow.com#{link['href']}") }
-    @data.map { |link| "https://stackoverflow.com#{link['href']}" }
+    extracted_data = @data.map { |link| {}.merge(
+      'title' => (link['title']).to_s,
+      'source_id' => link['href'].match(/\d+/),
+      'technology' => @tech,
+      'source' => 'StackOverFlow',
+      'link' => "https://stackoverflow.com#{link['href']}") }
+
+    JSON.pretty_generate(extracted_data)
+
+    # @data.map { |link| "https://stackoverflow.com#{link['href']}" }
   end
 
-  def json_file
-    json = JSON.pretty_generate(convert_data)
-    File.open("doc/#{@tech}-remote.json", 'w').write(json)
-  end
+  # def json_file
+  #   json = JSON.pretty_generate(convert_data)
+  #   File.open("doc/#{@tech}-remote.json", 'w').write(json)
+  # end
 end
 
-# jobs = Scraper.new("rails")
-# puts jobs.convert_data
+jobs = Scraper.new("rails")
+puts jobs.extract_data
